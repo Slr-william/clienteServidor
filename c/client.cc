9f9b4ee356a6e9a3533c7f9691f0d8a32d389475
slr-william <w.as.g@utp.edu.co>
2017-02-16 05:26:44 -0500

    finished with inteface

--- c/client.cc
+++ c/client.cc
@@ -2,11 +2,13 @@
 #include <string>
 #include <zmqpp/zmqpp.hpp>
 #include <fstream>
+#include <sys/stat.h>
+#include <dirent.h>
 
 using namespace std;
 using namespace zmqpp;
 
-void uploadfile(string name, socket *s){
+void uploadfile(string name, socket *s, const string &user){
     ifstream infile(name,ios::binary|ios::ate);
 
     message m;
@@ -21,25 +23,30 @@ void uploadfile(string name, socket *s){
     // read content of infile
     infile.read (buffer,size);
 
-    m <<"write"<< name<<size;
+    m <<"write"<< name<<size<<user;
     m.push_back(buffer,size);
     s->send(m);
 
     infile.close();
     delete[] buffer;
+
+    s->receive(m);
+    string aux;
+    m >> aux;
+    std::cout << aux << '\n';
   }
 
-void downloadfile(const string& name, socket *s){
+void downloadfile(const string& name, socket *s,const string &user){
     long size;
     message m;
     message data;
 
-    m <<"read"<< name;
+    m <<"read"<< name <<user;
     s->send(m);
 
     if(s->receive(data)){
       size = data.size(0);
-      std::cout << "The message arrive and his size is : " << size <<" parts: " << data.parts()<< '\n';
+      cout << "The message arrive and his size is : " << size <<" parts: " << data.parts()<< '\n';
 
       ofstream outfile(name,ios::binary);
       outfile.write((char*)data.raw_data(0),size);
@@ -54,18 +61,51 @@ int main() {
   socket s(ctx, socket_type::req);
 
   message m;
-  message answer;
-  string result;
+  message * answer = new message();
+  string nameFile;
   string user;
+  int n = 0;
 
   cout << "Connecting to tcp port 5555\n";
   s.connect("tcp://localhost:5555");
 
+  cout << "User: " << '\n';
+  cin >> user;
+  m <<"login"<< user;
+  s.send(m);
 
+  s.receive(*answer);
 
+  cout << "rECIBI mensaje con partes de : " << answer->parts()<< '\n';
+  string aux;
+  int size = answer->parts();
 
-  uploadfile("datos.jpg", &s);
-  downloadfile("parallelvsSequential.png", &s);
+  cout << "These are your files: " << '\n';
+  if(size <= 2){cout << "Your storage is empty" << '\n';}
+  else{
+    for (int i = 0; i < size; i++) {
+      *answer >> aux;
+      if(aux.compare(".") != 0 && aux.compare("..") != 0){
+        cout << i <<". "<<aux <<'\n';
+      }
+    }
+  }
+
+  cout <<endl<< "-For download a file put 1" << '\n';
+  cout << "-For upload a file put 2" << '\n'<<endl;
+  cin >> n;
+
+  switch (n) {
+    case 1:
+      cout << "Enter the file's name: " << '\n';
+      cin >> nameFile;
+      downloadfile(nameFile,&s,user);
+      break;
+    case 2:
+      cout << "Enter the file's name: " << '\n';
+      cin >> nameFile;
+      uploadfile(nameFile, &s, user);
+  }
 
   cout << "Finished\n";
   return 0;
--- s/server.cc
+++ s/server.cc
@@ -2,16 +2,38 @@
 #include <string>
 #include <zmqpp/zmqpp.hpp>
 #include <fstream>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <vector>
 
 using namespace std;
 using namespace zmqpp;
 
-void readfile (string name, socket &s){
+bool existsdir( const char* pzPath )
+{
+    if ( pzPath == NULL) return false;
+
+    DIR *pDir;
+    bool bExists = false;
+
+    pDir = opendir (pzPath);
+
+    if (pDir != NULL)
+    {
+        bExists = true;
+        (void) closedir (pDir);
+    }
+
+    return bExists;
+}
+
+void readfile (const string &name, socket &s, const string &user){
   message m;
   char * buffer;
   long size;
-
-  ifstream infile(name,ios::binary|ios::ate);
+  string path = "./users/"+user+"/"+name;
+  cout<<"path : " << path << '\n';
+  ifstream infile(path.c_str(),ios::binary|ios::ate);
   // get size of file
   size = infile.tellg();
   infile.seekg(0, infile.beg);
@@ -27,11 +49,39 @@ void readfile (string name, socket &s){
   delete[] buffer;
 }
 
-void writefile (string name, char * text, size_t size){
-  ofstream outfile (name ,ios::binary);
+message writefile (string name, char * text, size_t size, const string &user){
+  message m;
+  string path = "./users/"+user+"/"+name;
+  std::cout << path << '\n';
+  ofstream outfile (path.c_str() ,ios::binary);
   // write to outfile
   outfile.write(text, size );
   outfile.close();
+
+  m << "the file is save";
+  return m;
+}
+
+message login(const string & user) {
+
+  message data;
+  const char * path = ("./users/"+user).c_str();
+  cout << path << '\n';
+  if(!existsdir( path )){
+    mkdir(path, S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP | S_IXGRP | S_IROTH | S_IWOTH | S_IXOTH);
+  }
+
+  DIR* pDIR = opendir(path );
+  struct dirent* entry;
+
+  while(entry = readdir(pDIR)) {
+    cout << " while" << '\n';
+    cout << entry->d_name << '\n';
+    data.push_back(entry->d_name, ((string)(entry->d_name)).length());
+  }
+  closedir(pDIR);
+  //*data << "hola";
+  return (data);
 }
 
 string messageHandler(message &m, socket *s){
@@ -39,19 +89,29 @@ string messageHandler(message &m, socket *s){
   size_t size;
   string op;
   string nameFile;
+  string user;
 
   m >> op;
   m >> nameFile;
 
   cout << "Operation :" <<op<< '\n';
 
-  if (op == "read") {
-    readfile(nameFile, *s);
+  if (op == "login") {
+    message data = login(nameFile);
+    s->send(data);
+    return "You are logged";
+  }
+  else if(op == "read") {
+    m>>user;
+    std::cout << "/* message */ " <<user << '\n';
+    readfile(nameFile, *s, user);
     return "You have read";
   }
   else if(op == "write"){
     m >> size;
-    writefile(nameFile,(char * )m.raw_data(3), size);
+    m >> user;
+    message r = writefile(nameFile,(char * )m.raw_data(4), size, user);
+    s->send(r);
     return "You have written";
   }
   else{
